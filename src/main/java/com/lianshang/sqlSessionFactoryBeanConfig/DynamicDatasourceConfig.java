fdd6860baf840afa38aafbaeea1b909fce3b6ecc
longyun.sun <longyun.sun@lianshang.com>
2018-11-20 18:15:44 +0800

    单数据源默认起作用

--- src/main/java/com/lianshang/sqlSessionFactoryBeanConfig/DynamicDatasourceConfig.java
+++ src/main/java/com/lianshang/sqlSessionFactoryBeanConfig/DynamicDatasourceConfig.java
@@ -32,24 +32,26 @@ public class DynamicDatasourceConfig {
     	//用户自定义的jar包和数据源的映射
     	List<PkgDtsCfg> pkgDtsCfgs =  provider.getIfAvailable();
     	
-    	if(null != pkgDtsCfgs && !pkgDtsCfgs.isEmpty()){
     		int num = 0;
-    		for(PkgDtsCfg pkgDtsCfg : pkgDtsCfgs){
-    			num++;
-    			Map<String, String> config =  pkgDtsCfg.getConfig();
-    			Iterator<String> iterator =  config.keySet().iterator();
-    			while(iterator.hasNext()){
-    				String key  = iterator.next();
-    			String dataSourceName = config.get(key);
+		if (null != pkgDtsCfgs && !pkgDtsCfgs.isEmpty()) {
+			for(PkgDtsCfg pkgDtsCfg : pkgDtsCfgs){
+				num++;
+				Map<String, String> config =  pkgDtsCfg.getConfig();
+				Iterator<String> iterator =  config.keySet().iterator();
+				while(iterator.hasNext()){
+					String key  = iterator.next();
+					String dataSourceName = config.get(key);
 					DataSource dataSource = (DataSource) applicationContext.getBean(dataSourceName);
-    			if(null == dataSource){
-    				throw new RuntimeException("未找到名字为["+dataSourceName+"]的数据源");
-    			}
-    			targetDataSources.put(key, dataSource);
-    			DynamicDatasource.putKey(key);
-    			}
-    		}
-			if (num > 0 && !targetDataSources.values().isEmpty()) {
+					if(null == dataSource){
+						throw new RuntimeException("未找到名字为["+dataSourceName+"]的数据源");
+					}
+					targetDataSources.put(key, dataSource);
+					DynamicDatasource.putKey(key);
+				}
+			}
+		}
+
+		if (num > 0 && !targetDataSources.values().isEmpty()) {
 				//有数据源，则第一个设置为默认数据源
 				Object firstDataSource  = targetDataSources.values().iterator().next();
 				dynamicDataSource.setDefaultTargetDataSource(firstDataSource);
@@ -64,8 +66,6 @@ public class DynamicDatasourceConfig {
 					targetDataSources.put(DynamicDatasource.defaultKey, firstDataSource);
 				}
 			}
-    	}
-    
 		dynamicDataSource.setTargetDataSources(targetDataSources);
     	return dynamicDataSource;
     }

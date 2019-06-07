676ec7df24d1cfab1b232c7e49cdd1c0b92e44ec
longyun.sun <longyun.sun@lianshang.com>
2019-06-07 15:54:37 +0800

    代码生成器修改

--- src/main/java/com/lianshang/sqlSessionFactoryBeanConfig/PageHelperPropertyConfig.java
+++ src/main/java/com/lianshang/sqlSessionFactoryBeanConfig/PageHelperPropertyConfig.java
@@ -9,13 +9,13 @@ import lombok.Data;
 @Data
 public class PageHelperPropertyConfig {
 	
-    private String offsetAsPageNum;
-    private String rowBoundsWithCount;
-    private String reasonable;
-    private String pageSizeZero;
-    private String returnPageInfo;
-    private String supportMethodsArguments;
-    private String dialect;
-    private String params;
+    private String offsetAsPageNum="false";
+    private String rowBoundsWithCount="false";
+    private String reasonable="false";
+    private String pageSizeZero="false";
+    private String returnPageInfo="false";
+    private String supportMethodsArguments="true";
+    private String dialect="mysql";
+    private String params="false";
 
 }
